3f6d241f29f8f7497c2774fb452176af0b97050e
roc.du <roc.du@quectel.com>
2022-05-12 18:38:43 +0800

    demo code

--- src/main/java/com/quec/ApiApplication.java
+++ src/main/java/com/quec/ApiApplication.java
@@ -1,11 +1,14 @@
 package com.quec;
 
+import com.quec.demo.QueCloudDevUplinkDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(ApiApplication.class, args);
+        QueCloudDevUplinkDemo queCloudDevUplinkDemo = new QueCloudDevUplinkDemo();
+        queCloudDevUplinkDemo.msgUpLinkDemo();
     }
 }
