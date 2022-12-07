8ea9c286327509e5a16e81c1a6e0fea9515a409c
roc.du <roc.du@quectel.com>
2022-12-07 11:13:29 +0800

    demo code

--- src/main/java/com/quec/demo/QueCloudDevUplinkDemo.java
+++ src/main/java/com/quec/demo/QueCloudDevUplinkDemo.java
@@ -8,10 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 public class QueCloudDevUplinkDemo {
 
     public void msgUpLinkDemo() throws Exception {
-        AmqpClient amqpClient = new AmqpClient("24b9rucZxRMAkMhHbFSxPanj","6AYBWJYrv1qLrrCKfDvDw2TxQftGNLR3r8p4TA6y","amqps://iot-amqp.quectelcn.com:5671/quec-open","a.000000005503.aep_queue_41_20221101143859");
+        AmqpClient amqpClient = new AmqpClient("${accessKey}","${accessSecret}","${connectionUrl}","${queueName}");
         HandleMessage handleMessage = new HandleMessageImpl();
         amqpClient.start(handleMessage);
-        log.info("开始监听");
+        log.info("开始监听队列");
     }
 
 }
